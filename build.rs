c6a9f412718622e32853c90e7235d404bb30518b
taktik <taktik@renoise.com>
2025-05-15 23:45:35 +0200

    strip app and GUI functions from sokol

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() {
     // inject emscripten build options
     if std::env::var("TARGET").is_ok_and(|v| v.contains("emscripten")) {
-        println!("cargo::rustc-link-arg=-sUSE_WEBGL2");
-        // for libs: println!("cargo::rustc-link-arg=--no-entry");
+        println!("cargo::rustc-link-arg=-sINVOKE_RUN=0");
+        println!("cargo::rustc-link-arg=-sEXPORTED_FUNCTIONS=_init_audio,_shutdown_audio");
+        println!("cargo::rustc-link-arg=-sEXPORTED_RUNTIME_METHODS=ccall,cwrap");
+        println!("cargo::rustc-link-arg=--no-entry");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
 use std::ffi;
 
-use sokol::{app as sapp, audio as saudio, log as slog};
-
-const NUM_SAMPLES: usize = 32;
+use sokol::{
+    audio::{self as saudio},
+    log as slog,
+};
 
 // -------------------------------------------------------------------------------------------------
 
-struct State {
+#[derive(Debug, Default)]
+struct AudioState {
     pub even_odd: u32,
-    pub sample_pos: usize,
-    pub samples: [f32; NUM_SAMPLES],
 }
 
-impl Default for State {
-    fn default() -> Self {
-        Self {
-            even_odd: 0,
-            sample_pos: 0,
-            samples: [0.0; NUM_SAMPLES],
+// -------------------------------------------------------------------------------------------------
+
+extern "C" fn audio_callback(
+    buffer: *mut f32,
+    num_frames: i32,
+    num_channels: i32,
+    userdata: *mut core::ffi::c_void,
+) {
+    let state = unsafe { &mut *(userdata as *mut AudioState) };
+
+    let buf_slice: &mut [f32] = unsafe {
+        std::slice::from_raw_parts_mut(buffer, num_frames as usize * num_channels as usize)
+    };
+
+    for frame in buf_slice.chunks_exact_mut(num_channels as usize) {
+        state.even_odd += 1;
+        for sample in frame {
+            *sample = if 0 != (state.even_odd & 0x20) {
+                0.1
+            } else {
+                -0.1
+            };
         }
     }
 }
 
-extern "C" fn init(_user_data: *mut ffi::c_void) {
+#[no_mangle]
+pub extern "C" fn init_audio() {
+    let state = Box::new(AudioState::default());
+    let user_data = Box::into_raw(state) as *mut ffi::c_void;
+
     saudio::setup(&saudio::Desc {
+        num_channels: 2,
+        stream_userdata_cb: Some(audio_callback),
+        user_data,
         logger: saudio::Logger {
             func: Some(slog::slog_func),
             ..Default::default()
@@ -32,56 +55,14 @@ extern "C" fn init(_user_data: *mut ffi::c_void) {
     });
 }
 
-extern "C" fn frame(user_data: *mut ffi::c_void) {
-    let state = unsafe { &mut *(user_data as *mut State) };
-
-    let num_frames = saudio::expect();
-
-    for _ in 0..num_frames {
-        state.even_odd += 1;
-        state.sample_pos += 1;
-
-        if state.sample_pos == NUM_SAMPLES {
-            state.sample_pos = 0;
-            let _ = saudio::push(&(state.samples[0]), NUM_SAMPLES as _);
-        }
-
-        state.samples[state.sample_pos] = if 0 != (state.even_odd & 0x20) {
-            0.1
-        } else {
-            -0.1
-        };
-    }
-}
-
-extern "C" fn cleanup(user_data: *mut ffi::c_void) {
+#[no_mangle]
+pub extern "C" fn shutdown_audio() {
     saudio::shutdown();
-
-    let _ = unsafe { Box::from_raw(user_data as *mut State) };
+    let _ = unsafe { Box::from_raw(saudio::userdata() as *mut AudioState) };
 }
 
 // -------------------------------------------------------------------------------------------------
 
-pub fn main() {
-    let state = Box::new(State::default());
-    let user_data = Box::into_raw(state) as *mut ffi::c_void;
-
-    sapp::run(&sapp::Desc {
-        init_userdata_cb: Some(init),
-        frame_userdata_cb: Some(frame),
-        cleanup_userdata_cb: Some(cleanup),
-        user_data,
-        width: 640,
-        height: 480,
-        icon: sapp::IconDesc {
-            sokol_default: true,
-            ..Default::default()
-        },
-        window_title: c"audio test".as_ptr(),
-        logger: sapp::Logger {
-            func: Some(slog::slog_func),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
+fn main() {
+    // unused
 }
